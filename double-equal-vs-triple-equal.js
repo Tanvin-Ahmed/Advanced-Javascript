62223cfdb2a1420b3a115975c4f659423d46d6f2
Tanvin Ahmed <tanvinahmedtouhid91@gmail.com>
2021-02-02 01:13:17 +0600

    double-equal-vs-triple-equal

---
+++ double-equal-vs-triple-equal.js
@@ -0,0 +1,12 @@
+const number1 = 2
+const number2 = '2'
+if (number1 == number2) {
+    console.log('condition is true')
+} else {
+    console.log('condition is false')
+}
+if (number1 === number2) {
+    console.log('condition is true')
+} else {
+    console.log('condition is false')
+}
